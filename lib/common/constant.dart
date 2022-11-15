4dce5b9971f7b34d9159da595d1e8a042f11de10
Fadhil Muhammad <famuh79@gmail.com>
2022-11-15 23:07:04 +0700

    adding constant text and colors at constant.dart

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // COLOR
+const Color kPink = Color(0xEF6D6D);
+const Color kGreenBright = Color(0x6EE625);
+
+
+// text style
+final TextStyle kTitleScreen = 
+GoogleFonts.poppins(
+  fontSize: 20,
+  fontWeight: FontWeight.w400
+);
+
+final TextStyle kItemTitle = 
+GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600
+);
+
+final TextStyle kItemBody = 
+GoogleFonts.poppins(
+  fontSize: 10,
+  fontWeight: FontWeight.w400
+);
+
+
