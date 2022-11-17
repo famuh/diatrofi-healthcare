fb75926c311de1f769693f6b878f807c2e7e4111
Fadhil Muhammad <famuh79@gmail.com>
2022-11-17 10:40:47 +0700

    adding text style in constant

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -7,22 +7,38 @@ const Color kGreenBright = Color(0x6EE625);
 
 
 // text style
+final TextStyle kAuthScreen = GoogleFonts.poppins(
+  fontSize: 24,
+  fontWeight: FontWeight.bold
+);
+
+
 final TextStyle kTitleScreen = 
 GoogleFonts.poppins(
-  fontSize: 20,
-  fontWeight: FontWeight.w400
+  fontSize: 22,
+  fontWeight: FontWeight.w500
 );
 
-final TextStyle kItemTitle = 
+final TextStyle kSection = 
+GoogleFonts.poppins(
+  fontSize: 20,
+  fontWeight: FontWeight.w500 // medium
+);
+
+final TextStyle kBodyText = 
+GoogleFonts.poppins(
+  fontSize: 12,
+  // fontWeight: FontWeight.w400 // regular
+);
+
+
+final TextStyle kItemTittleCard = 
 GoogleFonts.poppins(
   fontSize: 14,
-  fontWeight: FontWeight.w600
+  fontWeight: FontWeight.w500 //semibold
 );
 
-final TextStyle kItemBody = 
-GoogleFonts.poppins(
-  fontSize: 10,
-  fontWeight: FontWeight.w400
-);
 
+
+// color scheme
 
