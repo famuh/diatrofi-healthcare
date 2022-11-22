d4583d6dc9c580e25340fa2f514a60b6c0062527
Fadhil Muhammad <famuh79@gmail.com>
2022-11-22 17:12:40 +0700

    menambahkan bagian-bagian lebih detail

--- lib/presentation/calculate_screen.dart
+++ lib/presentation/calculate_screen.dart
@@ -1,3 +1,5 @@
+import 'package:diatfori/widget/food_item_widget.dart';
+import 'package:diatfori/widget/sub_heading.dart';
 import 'package:flutter/material.dart';
 
 class CalculateScreen extends StatefulWidget {
@@ -14,40 +16,131 @@ class _CalculateScreenState extends State<CalculateScreen> {
     final mediaQuery = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('calculate your intake'),
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back_ios_rounded)),
-      ),
-      body: 
-      Container(
-        width: mediaQuery.width,
-        height: mediaQuery.height,
-        color: Colors.black,
-        child: Stack(
-          children: [
-            // 1
-            Container(
-              width: mediaQuery.width,
-              height: mediaQuery.height/1.3,
-              color: Colors.blueGrey,
-            ),
-
-            // 2
-            Positioned(
-              bottom: 0,
-              child: Container(
-                width: mediaQuery.width,
-                height: mediaQuery.height/7,
-                color: Colors.amber,
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: const Text('Calculate Your Intake'),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios_rounded)),
         ),
-      )
-    );
+        body: Container(
+          width: mediaQuery.width,
+          height: mediaQuery.height,
+          child: Stack(
+            children: [
+              // 1
+              Container(
+                width: mediaQuery.width,
+                height: mediaQuery.height / 1.3,
+                // color: Colors.blueGrey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 15,
+                  ),
+                  child: ListView(
+                    children: [
+                      SubHeading(
+                          title: 'foods',
+                          onTap: (() => print('ke halaman more'))),
+                      Container(
+                        height: 160,
+                        margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                          ],
+                        ),
+                      ),
+                     
+                      SubHeading(
+                          title: 'foods',
+                          onTap: (() => print('ke halaman more'))),
+                      Container(
+                        height: 160,
+                        margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                          ],
+                        ),
+                      ),
+                     
+                      SubHeading(
+                          title: 'foods',
+                          onTap: (() => print('ke halaman more'))),
+                      Container(
+                        height: 160,
+                        margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                            Container(
+                              width: 300,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              color: Colors.green,
+                            ),
+                          ],
+                        ),
+                      ),
+                     
+                    ],
+                  ),
+                ),
+              ),
+
+              // 2
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: mediaQuery.width,
+                  height: mediaQuery.height / 7,
+                  color: Colors.amber,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
