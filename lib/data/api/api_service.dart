ba05c36f1260dcc37543324a156c29ea359c14dd
Fadhil Muhammad <famuh79@gmail.com>
2022-11-26 15:07:17 +0700

    Article | API Service, model, provider

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:diatfori/data/model/article.dart';
 import 'package:diatfori/data/model/article_category.dart';
 import 'package:diatfori/data/model/article_resep.dart';
 import 'package:diatfori/data/model/article_resep_detail.dart';
@@ -37,30 +38,48 @@ class ApiService {
       throw Exception(_throw);
     }
   }
-  Future<ArticleResep> getArticleResep() async {
-    final response = await http.get(Uri.parse("${_baseUrl}/api/articles/new"));
-    if (response.statusCode == 200) {
-      return ArticleResep.fromJson(json.decode(response.body));
-    } else {
-      throw Exception(_throw);
-    }
-  }
 
-  Future<ArticleResepDetail> getArticleResepDetail(String tag, key) async {
-    final response = await http.get(Uri.parse("${_baseUrl}/api/article/:$tag/:$key"));
-    if (response.statusCode == 200) {
-      return ArticleResepDetail.fromJson(json.decode(response.body));
-    } else {
-      throw Exception(_throw);
-    }
-  }
+  // Future<ArticleResep> getArticleResep() async {
+  //   final response = await http.get(Uri.parse("${_baseUrl}api/articles/new"));
+  //   if (response.statusCode == 200) {
+  //     return ArticleResep.fromJson(json.decode(response.body));
+  //   } else {
+  //     throw Exception(_throw);
+  //   }
+  // }
 
-  Future<ArticleResepCategory> getArticleResepCategory() async {
-    final response = await http.get(Uri.parse('${_baseUrl}api/category/article'));
+  // Future<ArticleResepDetail> getArticleResepDetail(String tag, key) async {
+  //   final response = await http.get(Uri.parse("${_baseUrl}/api/article/:$tag/:$key"));
+  //   if (response.statusCode == 200) {
+  //     return ArticleResepDetail.fromJson(json.decode(response.body));
+  //   } else {
+  //     throw Exception(_throw);
+  //   }
+  // }
+
+  // Future<ArticleResepCategory> getArticleResepCategory() async {
+  //   final response = await http.get(Uri.parse('${_baseUrl}api/category/article'));
+  //   if (response.statusCode == 200) {
+  //     return ArticleResepCategory.fromJson(json.decode(response.body));
+  //   } else {
+  //     throw Exception(_throw);
+  //   }
+  // }
+  
+// https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=711d960e44b242d0b13100d48d6b3ec3
+  // article
+  static const String _articleBaseUrl = 'https://newsapi.org/v2/';
+  static const String _articleApiKey = '711d960e44b242d0b13100d48d6b3ec3';
+  static const String _articleCategory = 'health';
+  static const String _articleCountry = 'id'; 
+
+  Future<ArticlesResult> articleTopHeadline() async {
+    final response = await http.get(Uri.parse("${_articleBaseUrl}top-headlines?country=$_articleCountry&category=$_articleCategory&apiKey=$_articleApiKey"));
     if (response.statusCode == 200) {
-      return ArticleResepCategory.fromJson(json.decode(response.body));
+      return ArticlesResult.fromJson(json.decode(response.body));
     } else {
-      throw Exception(_throw);
+      throw Exception('Failed to load top headlines');
     }
   }
 }
+
