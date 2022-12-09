28940e02ac1dfa53baba6b4f893e65876e74ed1f
Fadhil Muhammad <famuh79@gmail.com>
2022-12-09 16:53:32 +0700

    menyelesaikan UI detail resep

--- lib/presentation/screen/item_detail_screen.dart
+++ lib/presentation/screen/item_detail_screen.dart
@@ -5,13 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
-import '../../data/model/food/food.dart';
 import '../../data/model/resep_detail.dart';
-import '../../widget/kcal_widget.dart';
+import '../../widget/expendable_text.dart';
 import 'food_recipe_screen.dart';
 
 class DetailScreen extends StatelessWidget {
-  static const ROUTE_NAME = '/resep-detail';
+  static const ROUTE_NAME = '/food-detail';
   String keyResep;
   DetailScreen({super.key, required this.keyResep});
 
@@ -41,6 +40,13 @@ class DetailScreen extends StatelessWidget {
                           )),
                       actions: [
                         IconButton(
+                          onPressed: () {
+                            print('Added to favorite');
+                          },
+                          icon: const Icon(Icons.favorite_border_rounded, size: 28,),
+                          
+                        ),
+                        IconButton(
                             onPressed: () {
                               Navigator.pushNamed(
                                   context, FoodRecipeScreen.ROUTE_NAME,
@@ -69,6 +75,7 @@ class DetailScreen extends StatelessWidget {
                       const SizedBox(
                         height: 20,
                       ),
+
                       // Penulis
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -83,7 +90,7 @@ class DetailScreen extends StatelessWidget {
                           ),
                           Text(
                             resep.author.datePublished,
-                            style: TextStyle(color: Colors.black54),
+                            style: const TextStyle(color: Colors.black54),
                           )
                         ],
                       ),
@@ -101,23 +108,23 @@ class DetailScreen extends StatelessWidget {
 
                       // tingkat kesulitan
                       Container(
-                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        margin: const EdgeInsets.only(top: 10),
                         padding: const EdgeInsets.symmetric(
                             vertical: 5, horizontal: 10),
                         decoration: const BoxDecoration(color: kSoftGreen),
                         child: Text(resep.difficulty),
                       ),
 
+                      // waktu memasak
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        decoration: const BoxDecoration(color: kSoftGrey),
+                        child: Text('Waktu memasak : ${resep.times}'),
+                      ),
+
                       // deskripsi
-                      // Container(
-                      //   margin: const EdgeInsets.symmetric(vertical: 10),
-                      //   width: mediaQuery.width,
-                      //   child: Text(
-                      //     resep.desc,
-                      //     maxLines: 4,
-                      //     overflow: TextOverflow.ellipsis,
-                      //   ),
-                      // ),
                       ExpandableText(resep.desc)
                     ],
                   ),
@@ -134,83 +141,6 @@ class DetailScreen extends StatelessWidget {
         ),
       ),
     );
-
-    // Scaffold(
-    //     body: Stack(
-    //   children: [
-    //     Container(
-    //       height: mediaQuery.height,
-    //       width: mediaQuery.width,
-    //       decoration: BoxDecoration(
-    //           color: Colors.grey,
-    //           borderRadius:
-    //               BorderRadius.only(bottomRight: Radius.circular(30))),
-    //       child: Stack(
-    //         children: [
-    //           Container(
-    //             width: mediaQuery.width,
-    //             decoration: BoxDecoration(
-    //               image: DecorationImage(
-    //                 image: NetworkImage('https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
-    //                 fit: BoxFit.cover,
-    //               ),
-    //             ),
-    //           ),
-
-    //           // ICON BUTTON
-    // Container(
-    //   width: mediaQuery.width,
-    //   margin: EdgeInsets.all(30),
-    //   height: 50,
-    //   // color: Colors.black,
-    //   child: Row(
-    //     children: [
-    //       customIconButton(Icon(Icons.arrow_back_ios_new_rounded),
-    //           () => {print('tapped !')}),
-    //       const Spacer(),
-    //       customIconButton(
-    //           FaIcon(
-    //             FontAwesomeIcons.fire,
-    //           ),
-    //           () => {print('tapped !')}),
-    //       const SizedBox(
-    //         width: 8,
-    //       ),
-    //       customIconButton(
-    //           Icon(Icons.add), () => {print('tapped !')}),
-    //     ],
-    //   ),
-    // ),
-    //     ],
-    //   ),
-    // ),
-    //     Positioned(
-    //       bottom: 0,
-    //       child: Container(
-    //         height: mediaQuery.height / 2.3,
-    //         width: mediaQuery.width,
-    //         padding: EdgeInsets.symmetric(
-    //           vertical: 0,
-    //           horizontal: 20
-    //         ),
-    //         decoration: BoxDecoration(
-    //             color: Colors.white,
-    //             borderRadius: BorderRadius.only(topLeft: Radius.circular(50))),
-    //             child: ListView(
-    //               scrollDirection: Axis.vertical,
-    //               children: [
-    //                 Container(
-    //                   height: 80,
-    //                   color: Colors.green,
-    //                 )
-    //               ],
-    //             ),
-    //       ),
-    //     ),
-    //   ],
-    // )
-
-    // );
   }
 
   InkWell customIconButton(icon, Function() onTap) {
@@ -224,40 +154,5 @@ class DetailScreen extends StatelessWidget {
         child: icon,
       ),
     );
-  }
-}
-
-class ExpandableText extends StatefulWidget {
-  ExpandableText(this.text);
-
-  final String text;
-  bool isExpanded = false;
-
-  @override
-  _ExpandableTextState createState() => new _ExpandableTextState();
-}
-
-class _ExpandableTextState extends State<ExpandableText>
-    with TickerProviderStateMixin<ExpandableText> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-      AnimatedSize(
-          duration: const Duration(milliseconds: 500),
-          child: ConstrainedBox(
-              constraints: widget.isExpanded
-                  ? BoxConstraints()
-                  : BoxConstraints(maxHeight: 50.0),
-              child: Text(
-                widget.text,
-                softWrap: true,
-                overflow: TextOverflow.fade,
-              ))),
-      widget.isExpanded
-          ? ConstrainedBox(constraints: BoxConstraints())
-          : TextButton(onPressed: () => setState(() => widget.isExpanded = true), child: Text('selengkapnya'))
-    ]);
   }
 }
